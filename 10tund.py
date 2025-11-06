5e9b699a79ea65fb37f0a81a4db2614d1278e898
gerdasiitan <gerda.siitan13@gmail.com>
2025-11-06 13:12:05 +0200

    fafsdf

--- 10tund.py
+++ 10tund.py
@@ -1,6 +1,9 @@
+import yuh
+
 def read_from_file(file_path):
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
     return content
 
 read_from_file("todo.txt")
+add_to_file("todo.txt")
